02de517c75d74ad2b358c531beba94e50f539bff
Valeriy <the.akopyan.valeriy@gmal.com>
2021-02-18 15:12:05 +0300

    Final save

--- Assets/GameStarter.cs
+++ Assets/GameStarter.cs
@@ -7,19 +7,23 @@ public class GameStarter : MonoBehaviour
 {
     private InputManager _inputManager;
     [SerializeField] private PlayerView playerView;
-    [SerializeField] private BoxView[] boxViews;
     void Start()
     {
+        var boxViews = FindObjectsOfType<BoxView>();
+        
         _inputManager = new InputManager();
+        
         var playerModel = new PlayerModel();
         var playerViewModel = new PlayerViewModel(playerModel);
-        playerView.Initialize(playerViewModel);
+        
         foreach (var box in boxViews)
         {
             var boxModel = new BoxModel();
             var boxViewModel = new BoxViewModel(boxModel);
             box.Initialize(boxViewModel);
         }
+        
+        playerView.Initialize(playerViewModel);
     }
     void Update()
     {
